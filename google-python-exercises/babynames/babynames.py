bf60414b395d1100b325d76da5ab284927e717c7
Daniel Miller <daniel.miller@openbet.com>
2019-04-11 17:38:06 +0100

    baby names regex completed

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -11,15 +11,41 @@ import re
 
 def extract_names(filename):
 	
-	naming_data = []
+	naming_data = {}
 	file_stream = open(filename, 'rU')
 
 	for line in file_stream:
 		data = re.findall(r'<td>(\w+)</td>', line)
-		print data
+		if (len(data) == 3):
+			naming_data[data[1]] = data[0]
+			naming_data[data[2]] = data[0]
 
-	return
+	file_stream.close()
+	return naming_data
 
+def report_summary(filename, file_year, names):
+	open_stream = open(filename+'.summary', 'w+')
+	open_stream.write('%s\n' %file_year)
+
+	for name in names:
+		open_stream.write('%s: %s\n' %(name[0], name[1]))
+
+	open_stream.close()
+
+def console_print(file_year, names):
+	print file_year
+	for name in names:
+		print '%s: %s' %(name[0], name[1])
+
+def return_data(names, filename, summary):
+
+	file_year = ''.join(re.findall(r'(\d+)', filename))
+
+	if summary == False:
+		console_print(file_year, names)
+	else:
+		report_summary(filename, file_year, names)
+	
 
 def main():
 	# This command-line parsing code is provided.
@@ -37,7 +63,10 @@ def main():
 		summary = True
 		del args[0]
 
-	extract_names(args[0])
+	name_data = extract_names(args[0])
+	return_data(sorted(name_data.items()), args[0], summary)
 
 if __name__ == '__main__':
 	main()
+
+
