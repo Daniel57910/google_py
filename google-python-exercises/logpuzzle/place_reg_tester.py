47847a6eed1e76991e3d056e72a606e105aacca2
Daniel Miller <danielmiller5791@gmail.com>
2019-05-04 15:01:04 +0100

    python exercises completed

--- google-python-exercises/logpuzzle/place_reg_tester.py
+++
@@ -1,7 +0,0 @@
-import sys
-import re
-example = 'http: // code.google.com/edu/languages/google-python-class/images/puzzle/p-bija-baei.jpg'
-reg = r'\p-(\w+)-(\w+)'
-
-word = re.search(reg, example).group(2)
-print(word)
